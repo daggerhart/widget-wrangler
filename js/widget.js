444f2480f8f75af547a52b99b5141a4b91fa9140
Jonathan Daggerhart <jonathan@daggerhart.com>
2011-11-25 06:44:04 -0800

    Added some widget area js

---
+++ js/widget.js
@@ -0,0 +1,25 @@
+/*
+ * Simple show/hide toggle for field options
+ */
+function ww_field_options_toggle(element) {
+  if (jQuery(element).is(':checked')){
+    jQuery(element).parent().parent().siblings('.ww-options-group-content').show();
+  }
+  else {
+    jQuery(element).parent().parent().siblings('.ww-options-group-content').hide();
+  }
+}
+
+jQuery(document).ready(function(){
+  // Field Options
+  jQuery('.ww-options-group-title input[type=checkbox]').click(function(){
+    ww_field_options_toggle(jQuery(this));
+  });
+  
+  // toggle selected fields
+  jQuery('.ww-options-hidden').each(function(index,element){
+    if(jQuery(element).parent().find('.ww-options-group-title input[type=checkbox]').is(':checked')){
+      jQuery(element).removeClass('ww-options-hidden');
+    }
+  });  
+});
