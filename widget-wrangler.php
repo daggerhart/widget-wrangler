4d1e1846fb56b89f1126251d43c9d5cb513757d4
Jonathan Daggerhart <jonathan@daggerhart.com>
2012-02-10 07:04:31 -0800

    upgrade script

--- widget-wrangler.php
+++ widget-wrangler.php
@@ -8,9 +8,10 @@ Version: 2.0
 Author URI: http://www.daggerhart.com
 License: GPL2
 */
+define('WW_VERSION', 2.0);
 /*  Copyright 2010  Jonathan Daggerhart  (email : jonathan@daggerhart.com)
   This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License, version 2, as 
+  it under the terms of the GNU General Public License, version 2, as
   published by the Free Software Foundation.
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -22,7 +23,6 @@ License: GPL2
 */
 define('WW_PLUGIN_DIR', dirname(__FILE__));
 define('WW_PLUGIN_URL', get_bloginfo('wpurl')."/wp-content/plugins/widget-wrangler");
-define('WW_VERSION', 2.0);
 
 // functions
 include_once WW_PLUGIN_DIR.'/functions.inc';
@@ -37,31 +37,53 @@ include_once WW_PLUGIN_DIR.'/post_type.widget.inc';
 // include WP standard widgets for sidebars
 include_once WW_PLUGIN_DIR.'/widget.sidebar.inc';
 
-// include install inc
-include_once WW_PLUGIN_DIR.'/includes/install.inc';
-
-// activation hooks
-//register_activation_hook(__FILE__, 'ww_post_widgets_table');
-//register_activation_hook(__FILE__, 'ww_widget_data_table');
-//register_activation_hook(__FILE__, 'ww_widget_spaces_table');
-//register_activation_hook(__FILE__, 'ww_widget_space_term_relationships_table');
-//register_activation_hook(__FILE__, 'ww_default_spaces');
 /*
- * Admin initialize 
+ * checkout version number and perform an upgrade if necessary
+ */
+function ww_check_version(){
+  if($last_version = get_option('ww_version'))
+  {
+    // compare versions
+    if ($last_version < WW_VERSION)
+    {
+      add_action('admin_notices', 'ww_upgrade_message');
+      //// include upgrade inc
+      //include_once WW_PLUGIN_DIR.'/includes/upgrade.inc';
+      //$upgrade_function = 'ww_upgrade_'.ww_make_slug($last_version).'_to_'.ww_make_slug(WW_VERSION);
+      //if(function_exists($upgrade_function)){
+      //  $upgrade_function();
+      //}
+      //update_option('ww_version', WW_VERSION);
+    }
+  }
+  // if last version doesn't exist, then its an old 1.x version
+  else
+  {
+    add_action('admin_notices', 'ww_upgrade_message');
+    //// include upgrade inc
+    //include_once WW_PLUGIN_DIR.'/includes/upgrade.inc';
+    //// run version related upgrade
+    //ww_upgrade_1x_to_2x();
+    //update_option('ww_version', WW_VERSION);
+  }
+}
+
+/*
+ * Admin initialize
  */
 function ww_admin_init()
 {
   // handle upgrades
   ww_check_version();
-  
+
   // include admin panel and helper functions such as sortable widgets
   include_once WW_PLUGIN_DIR.'/includes/admin-panel.inc';
-  
+
   // determine whether to display the admin panel
   // handles adding some css and the js
   ww_display_admin_panel();
 
-  // add admin css  
+  // add admin css
   add_action( 'admin_head', 'ww_admin_css');
 
 }
@@ -99,10 +121,10 @@ function ww_spaces_page_handler()
       case 'create':
         $space_id = ww_create_space();
         break;
-      
+
       // update an existing widget space
       case 'update':
-        
+
         // switch Save & Delete buttons
         // do not let them delete defaults or post pages
         if(isset($_POST['action-delete']) &&
@@ -118,7 +140,7 @@ function ww_spaces_page_handler()
     }
     // send to the new space
     wp_redirect(get_bloginfo('wpurl').'/wp-admin/edit.php?post_type=widget&page=ww-spaces&space_id='.$space_id);
-    
+
   }
   else {
     // spaces edit form
@@ -132,7 +154,7 @@ function ww_sidebars_page_handler()
 {
   // include the sidebars form
   include_once WW_PLUGIN_DIR.'/includes/sidebars.inc';
-  
+
   if($_GET['ww-sidebar-action']){
     switch($_GET['ww-sidebar-action']){
       case 'insert':
@@ -159,7 +181,7 @@ function ww_sidebars_page_handler()
 function ww_clone_page_handler()
 {
   include_once WW_PLUGIN_DIR.'/includes/clone.inc';
-  
+
   if($_GET['ww-clone-action']){
     switch($_GET['ww-clone-action']){
       case 'insert':
@@ -179,7 +201,7 @@ function ww_clone_page_handler()
  * Handles settings page
  */
 function ww_settings_page_handler()
-{ 
+{
   if ($_GET['ww-settings-action']){
     switch($_GET['ww-settings-action']){
       case "save":
@@ -189,7 +211,7 @@ function ww_settings_page_handler()
         ww_settings_reset_widgets();
         break;
     }
-    wp_redirect(get_bloginfo('wpurl').'/wp-admin/edit.php?post_type=widget&page=ww-settings');  
+    wp_redirect(get_bloginfo('wpurl').'/wp-admin/edit.php?post_type=widget&page=ww-settings');
   }
   else{
     // settings form
@@ -199,18 +221,9 @@ function ww_settings_page_handler()
 /*
  * for whatever.
  */
-function ww_debug_page(){
-  /*/global $wp_registered_widgets, $wp_registered_widget_controls, $wp_registered_widget_updates, $_wp_deprecated_widgets_callbacks;
-  //global $wp_widget_factory,$wp_registered_widgets, $wpdb;
-  global $wp_filter;
-  //print_r($wp_filter);
-  foreach($wp_filter as $k => $v){
-    print $k."<br>";
-  }
-  // */
-}
+function ww_debug_page(){}
+// */
 /*********************************************** end page handling */
-
 /*
  * Shortcode support for all widgets
  *
@@ -231,4 +244,146 @@ function ww_hec_show_dbx( $to_show )
 {
   array_push( $to_show, 'widget-wrangler' );
   return $to_show;
+}
+
+
+/*******************************************************************************
+ * Activation hooks
+ *
+ * These are required to be in this file
+ */
+
+// activation hooks
+register_activation_hook(__FILE__, 'ww_post_widgets_table');
+register_activation_hook(__FILE__, 'ww_widget_data_table');
+register_activation_hook(__FILE__, 'ww_widget_spaces_table');
+register_activation_hook(__FILE__, 'ww_widget_space_term_relationships_table');
+register_activation_hook(__FILE__, 'ww_default_spaces');
+/*
+ * Create post - widgets table
+ */
+function ww_post_widgets_table(){
+  global $wpdb;
+  $table = $wpdb->prefix."ww_post_widgets";
+
+  $sql = "CREATE TABLE " . $table . " (
+	  post_id mediumint(11) NOT NULL,
+	  widgets text NOT NULL,
+	  UNIQUE KEY id (post_id)
+  );";
+
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
+}
+/*
+ * Create widget data table
+ */
+function ww_widget_data_table(){
+  global $wpdb;
+  $table = $wpdb->prefix."ww_widget_data";
+
+  $sql = "CREATE TABLE " . $table . " (
+	  post_id mediumint(11) NOT NULL,
+	  type varchar(32) NOT NULL,
+   data text NOT NULL,
+   UNIQUE KEY id (post_id)
+  );";
+
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
+}
+/*
+ * Create widget spaces table
+ */
+function ww_widget_spaces_table(){
+  global $wpdb;
+  $table = $wpdb->prefix."ww_widget_spaces";
+
+  $sql = "CREATE TABLE " . $table . " (
+	  id mediumint(11) NOT NULL AUTO_INCREMENT,
+	  type varchar(32) NOT NULL,
+   data text NOT NULL,
+   widgets text NOT NULL,
+   UNIQUE KEY id (id)
+  );";
+
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
+}
+/*
+ * Create widget spaces table
+ */
+function ww_widget_space_term_relationships_table(){
+  global $wpdb;
+  $table = $wpdb->prefix."ww_space_term_relationships";
+
+  $sql = "CREATE TABLE " . $table . " (
+	  space_id mediumint(11) NOT NULL,
+	  term_id mediumint(11) NOT NULL
+  );";
+
+  require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+  dbDelta($sql);
+}
+/*
+ * Look for an insert default spaces
+ */
+function ww_default_spaces(){
+  global $wpdb;
+  $table = $wpdb->prefix."ww_widget_spaces";
+  $data = array(
+    'id' => 0,
+    'type' => 'default',
+    'data' => '',
+    'widgets' => serialize(array()),
+  );
+
+  // defaults
+  $sql_select = "SELECT id FROM ".$table." WHERE id = 1";
+  $row = $wpdb->get_row($sql_select);
+  if(!$row){
+    $data['id'] = 1;
+    $data['data'] = serialize(array('name' => 'Defaults'));
+    $wpdb->insert($table, $data);
+  }
+
+  // posts page
+  $sql_select = "SELECT id FROM ".$table." WHERE id = 2";
+  $row = $wpdb->get_row($sql_select);
+  if(!$row){
+    $data['id'] = 2;
+    $data['data'] = serialize(array('name' => 'Posts Page'));
+    $wpdb->insert($table, $data);
+  }
+
+  // search page
+  $sql_select = "SELECT id FROM ".$table." WHERE id = 3";
+  $row = $wpdb->get_row($sql_select);
+  if(!$row){
+    $data['id'] = 3;
+    $data['data'] = serialize(array('name' => 'Search Page'));
+    $wpdb->insert($table, $data);
+  }
+
+  // 404 page
+  $sql_select = "SELECT id FROM ".$table." WHERE id = 4";
+  $row = $wpdb->get_row($sql_select);
+  if(!$row){
+    $data['id'] = 4;
+    $data['data'] = serialize(array('name' => '404 Page'));
+    $wpdb->insert($table, $data);
+  }
+}
+
+// http://www.wprecipes.com/how-to-show-an-urgent-message-in-the-wordpress-admin-area
+function ww_set_message($message, $type = 'updated')
+{
+		 return '<div id="message" class="'.$type.'">
+             <p>'.$message.'</p>
+           </div>';
+}
+function ww_upgrade_message(){
+  $msg = '<strong>Widget Wrangler requires a database update!</strong><br />
+          Backup your database, and then visit <a href="">this page</a> to perform the update.';
+  print ww_set_message($msg, 'error');
 }
